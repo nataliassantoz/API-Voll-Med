e07588db4840ecc1d1bad3686c2dfe855c45381a
Natália Santos <nataliassantosf@gmail.com>
2025-07-16 13:20:45 -0300

    chore:criaçao de dependencias

---
+++ src/main/java/med/voll/api_CRUD/ApiCrudApplication.java
@@ -0,0 +1,13 @@
+package med.voll.api_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudApplication.class, args);
+	}
+
+}
